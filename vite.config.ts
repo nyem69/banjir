dbf8e4bbcaccef9bbcde015f7c1c69ad35840096
Nyem <nyem69@gmail.com>
2024-12-12 12:40:32 +0800

    compatibility_flags

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { paraglide } from "@inlang/paraglide-sveltekit/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { paraglide } from "@inlang/paraglide-sveltekit/vite";
 
 export default defineConfig({
 	plugins: [
